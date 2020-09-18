2b2974108d1cd403d0433b1b6b916d81ca3509a2
CoderYihaoWang <yesiamyihao@gmail.com>
2020-09-18 14:02:50 +1200

    Add action method stubs

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const program = require('commander')
 const package = require('./package.json')
 
 function main() {
-    program.version(package.version)
-    .parse(process.argv)
+    program.version(package.version, '-v, --version')
 
+    
+
+
+
+
+    program.parse(process.argv)
 }
 
 module.exports = main
---
+++ src/actions.js
@@ -0,0 +1,25 @@
+module.exports = {
+    update: function() {
+
+    },
+
+    query: function() {
+
+    },
+
+    queryInName: function() {
+
+    },
+
+    queryInContent: function() {
+
+    },
+
+    list: function() {
+
+    },
+
+    random: function() {
+
+    }
+}
