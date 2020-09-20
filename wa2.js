562ec2b4b73bfca7a01c12638dcf259da358f3ac
CoderYihaoWang <yesiamyihao@gmail.com>
2020-09-20 13:28:51 +1200

    Add descriptions

--- wa2.js
+++ wa2.js
@@ -7,27 +7,28 @@ function main() {
         .name('wa2')
 
     program.arguments('<query...>')
-        .usage('<query...> - query for the specified service or its abbreviation in AWS glossary')
-        .option('-t, --title', 'search for items containing the queried word(s) in the title')
-        .option('-c, --content', 'search for items containing the queried word(s) in the content')
+        .usage('[options] <query...>')
+        .description('Query for the specified service or its abbreviation in AWS glossary')
+        .option('-t, --title', 'Search for items containing the queried word(s) in the title')
+        .option('-c, --content', 'Search for items containing the queried word(s) in the content')
         .action(actions.query)
 
     program.command('update')
-        .usage('- generate a new glossary list from the AWS glossary page, to keep the search results up to date')
-        .option('-l, --list', 'list all the available versions of glossaries')
-        .option('-r, --revert [id]', 'revert to a particular version of glossary, or the last one if [id] is not given')
+        .description('Generate a new glossary list from the AWS glossary page, to keep the search results up to date')
+        .option('-l, --list', 'List all the available versions of glossaries')
+        .option('-r, --revert [id]', 'Revert to a particular version of glossary, or the last one if [id] is not given')
         .action(actions.update)
 
     program.command('random')
-        .usage('- show a random item')
+        .description('Show a random item')
         .action(actions.random)
 
     program.command('list [letter]')
-        .usage('[initial letter] - list items with the initial letter')
+        .description('List items with the initial letter, or all services if the initial is not given')
         .action(actions.list)
 
     program.command('acronym <query>')
-        .usage('search for items with the specified acronym')
+        .description('Search for items with the specified acronym')
         .action(actions.acronym)
 
     program.parse(process.argv)
